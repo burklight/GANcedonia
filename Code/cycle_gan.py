1dfb64795446dec1e6ebd06f544333b9e46376ef
Miquel Larsson <miquellarsson@gmail.com>
2018-05-10 20:07:31 +0200

    Small mistake with logging function

--- Code/cycle_gan.py
+++ Code/cycle_gan.py
@@ -326,6 +326,7 @@ losses_filepath=os.path.join(filepath, losses_log)
 
 with open(losses_filepath, "a") as file:
 	csv_header=['Epoch', 'Timestamp', 'loss_identity_A', 'loss_identity_B', 'loss_identity','loss_gan_AB', 'loss_gan_BA', 'loss_gan', 'loss_cycle_A','loss_cycle_B','loss_cycle','loss_G', 'loss_real_A', 'loss_real_B', 'loss_fake_A','loss_fake_B','loss_D_A','loss_D_B','loss_D']
+	writer = csv.writer(losses_filepath, delimiter=',')
 	writer.writerow(csv_header)
 
 def log_losses(losses_list, losses_filepath):
